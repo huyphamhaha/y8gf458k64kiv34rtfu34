55b45e1686ce291c7073b11a57db9b528a3e4070
huyphamhaha <phamhuynhgiahuy1511@gmail.com>
2024-01-13 19:43:47 +0700

    uploadSourceCode

---
+++ app.js
@@ -0,0 +1,12 @@
+
+// nav
+
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+    if(scrollY >= 188){
+        navbar.classList.add('bg');
+    } else{
+        navbar.classList.remove('bg')
+    }
+})
