19bbb1665f06d9bd82b0551ec28cca24f69c428b
huyphamhaha <phamhuynhgiahuy1511@gmail.com>
2024-01-14 18:53:35 +0700

    SlideShowUpload

--- app.js
+++ app.js
@@ -10,3 +10,36 @@ window.addEventListener('scroll', () => {
         navbar.classList.remove('bg')
     }
 })
+
+//Slide Show
+document.addEventListener("DOMContentLoaded", function () {
+    var images = [
+        "./Img/background1.jpg",
+        "./Img/background2.jpg",
+        "./Img/background3.jpg",
+        "./Img/background4.jpg",
+        "./Img/background5.jpg",
+        "./Img/background6.jpg",
+        "./Img/background7.jpg",
+        "./Img/background8.jpg",
+        "./Img/background9.jpg",
+        "./Img/background10.jpg",
+
+    ]; // Thêm các đường dẫn của hình ảnh muốn chuyển đổi
+    var imageElement = document.getElementById("backgroundSlide");
+    var currentIndex = 0;
+
+    function changeImage() {
+        currentIndex = (currentIndex + 1) % images.length;
+        var nextImage = images[currentIndex];
+
+        imageElement.style.opacity = 0;
+
+        setTimeout(function () {
+            imageElement.src = nextImage;
+            imageElement.style.opacity = 1;
+        }, 1500); // 1000 milliseconds = 1 second
+    }
+
+    setInterval(changeImage, 7000); // 7000 milliseconds = 7 seconds
+});
